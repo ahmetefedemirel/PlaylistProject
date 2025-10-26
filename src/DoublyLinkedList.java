589659849c6b81ebae063becfc6f3e350a305761
Ahmet Efe Demirel <ahmetefeed@gmail.com>
2025-10-26 17:00:00 +0300

    Playlist Project

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,4 +1,73 @@
-package PACKAGE_NAME;
-
 public class DoublyLinkedList {
+    private Song head;
+    private Song tail;
+    private Song current;
+
+    public void addSong(String title){
+        Song newSong = new Song(title);
+
+        if(head == null){
+            head = newSong;
+            tail = newSong;
+            current = newSong;
+        }
+        else{
+            tail.next = newSong;
+            newSong.prev = tail;
+            tail = newSong;
+        }
+    }
+
+
+    public void nextSong(){
+        if (current != null && current.next != null){
+            current = current.next;
+            System.out.println("Now playing " + current.title);
+        }
+        else{
+            System.out.println("End of playlist.");
+        }
+    }
+
+    public void previousSong(){
+        if (current != null && current.prev != null){
+            current = current.prev;
+            System.out.println("Now playing " + current.title);
+        }
+        else{
+            System.out.println("Start of playlist.");
+        }
+    }
+
+
+    public void printPLaylist(){
+        Song temp = head;
+        while(temp != null){
+            System.out.println(temp.title);
+            System.out.println("...");
+            temp = temp.next;
+        }
+    }
+
+
+    public void removeCurrent(){
+        if(current == null){
+            System.out.println("Removing " + current.title);
+        }
+
+        if(current.prev != null){
+            current.prev.next = current.next;
+        }
+        head = current.next;
+
+        if(current.next != null){
+            current.next.prev = current.prev;
+        }
+        tail = current.prev;
+    }
+
+    public void playCurrent(){
+        System.out.println("Playing " + current.title);
+    }
+
 }
--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Song {
+    String title;
+    Song next;
+    Song prev;
+
+
+    public Song(String title){
+        this.title = title;
+        this.next = null;
+        this.prev = null;
+    }
 }
